0707ade3623b864d71db9cf003441702e604f383
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-20 21:00:43 +0530

    fix: errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:app/chat/chat.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});

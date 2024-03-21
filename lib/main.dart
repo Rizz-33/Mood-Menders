3fc6734c4624f93ae8f2d8d161e24049698f88e7
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-21 06:50:48 +0530

    modify: main to display firebase connected or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
-import 'package:app/chat/chat.dart';
+import 'package:app/chat/chat.dart'; // Make sure this import is correct
+import 'package:app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  try {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  } catch (e) {
+    print('Error initializing Firebase: $e');
+  }
+  runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-        //home: loading_page()
-        home: ChatPage(receiverEmail: 'aarruwanthie@gmail.com', receiverID: 'tDsSD8nQySbmUwL7nNjuKL3GRp22')
-        );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatPage(
+        receiverEmail: 'aarruwanthie@gmail.com',
+        receiverID: 'tDsSD8nQySbmUwL7nNjuKL3GRp22',
+      ),
+    );
   }
 }

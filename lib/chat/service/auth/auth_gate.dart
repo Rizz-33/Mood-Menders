9de20e631a2e4bca728a0026cf3a78e1bbebec53
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-21 09:55:37 +0530

    fix: imports in auth gate

--- lib/chat/service/auth/auth_gate.dart
+++ lib/chat/service/auth/auth_gate.dart
@@ -1,4 +1,5 @@
 import 'package:app/chat/home.dart';
+import 'package:app/chat/service/auth/login_or_register.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 

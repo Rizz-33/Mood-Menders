93afeefe605d69ba5801f96948a1c1ca7bf7c889
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-23 12:28:01 +0530

    feat: user details in profile

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,186 +1,202 @@
-import 'package:app/chat/service/auth/auth_service.dart';
 import 'package:app/profile/editProfile.dart';
 import 'package:app/profile/feedback.dart';
 import 'package:app/profile/inviteFriends.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class Profile extends StatelessWidget {
+import '../chat/service/auth/auth_service.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  String userName = '';
+  String userEmail= '';
 
   void logout(){
-    //get auth service
     final auth = AuthService();
     auth.signOut();
   }
-  
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Padding(
-            padding: EdgeInsets.only(right: 16.0),
-            child: Text('Profile'),
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 16.0),
-              child: Image.asset('lib/images/1.png'),
-            ),
-          ],
+
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await FirebaseFirestore.instance
+              .collection('Users')
+              .doc(user.uid)
+              .get();
+      if (snapshot.exists) {
+        setState(() {
+          userName = snapshot.data()!['name'];
+          userEmail = snapshot.data()!['email'];
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.only(right: 16.0),
+          child: Text('Profile'),
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.person, size: 100, color: Color.fromARGB(255, 84, 166, 166),),
-                const SizedBox(height: 8.0),
-                const Text(
-                  'John Doe',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                const Text(
-                  'aarruwanthie@gmail.com',
-                  style: TextStyle(
-                    fontSize: 13,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                const SizedBox(height: 36.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => EditProfile()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black, backgroundColor: Colors.white,
-                      side: const BorderSide(color: Colors.black, width: 0.4),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+                  children: [
+                    Icon(Icons.person, size: 100, color: Color.fromARGB(255, 84, 166, 166)),
+                    const SizedBox(height: 8.0),
+                    Text(userName, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 8.0),
+                    Text(userEmail, style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal)),
+                    const SizedBox(height: 36.0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => EditProfile()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black, backgroundColor: Colors.white,
+                          side: const BorderSide(color: Colors.black, width: 0.4),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          elevation: 0,
+                          minimumSize: const Size(120, 40),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Edit Profile'),
+                            Icon(Icons.arrow_forward),
+                          ],
+                        ),
                       ),
-                      elevation: 0,
-                      minimumSize: const Size(120, 40),
                     ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Edit Profile'),
-                        Icon(Icons.arrow_forward),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => FeedbackForm()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black, backgroundColor: Colors.white,
-                      side: const BorderSide(color: Colors.black, width: 0.4),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                    const SizedBox(height: 16.0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => FeedbackForm()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black, backgroundColor: Colors.white,
+                          side: const BorderSide(color: Colors.black, width: 0.4),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          elevation: 0,
+                          minimumSize: const Size(120, 40),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Give us Feedback'),
+                            Icon(Icons.arrow_forward),
+                          ],
+                        ),
                       ),
-                      elevation: 0,
-                      minimumSize: const Size(120, 40),
                     ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Give us Feedback'),
-                        Icon(Icons.arrow_forward),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => InviteFriends()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black, backgroundColor: Colors.white,
-                      side: const BorderSide(color: Colors.black, width: 0.4),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                    const SizedBox(height: 16.0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => InviteFriends()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black, backgroundColor: Colors.white,
+                          side: const BorderSide(color: Colors.black, width: 0.4),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          elevation: 0,
+                          minimumSize: const Size(120, 40),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Invite Friends'),
+                            Icon(Icons.arrow_forward),
+                          ],
+                        ),
                       ),
-                      elevation: 0,
-                      minimumSize: const Size(120, 40),
                     ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Invite Friends'),
-                        Icon(Icons.arrow_forward),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-              const SizedBox(height: 36.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: GestureDetector(
+                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 36.0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: GestureDetector(
                         onTap: logout,
                         child: Container(
-                            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                gradient: const LinearGradient(colors: <Color>[
-                  Color(0xFF77CB7E),
-                  Color(0xFFB4CAE8),
-                ]),
-                            ),
-                            padding: const EdgeInsets.all(20),
-                            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    ('Logout'),
-                    style: const TextStyle(
-                        color: const Color.fromARGB(255, 70, 66, 68),
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-                            ))),
-              ),
-              SizedBox(height: 50,),
-              Padding(
-                padding: const EdgeInsets.only(left: 16),
-                child: Row(
-                  children: [
-                    SizedBox(height: 50,),
-                    Text('Developed by undergraduate students\nfrom University of Plymouth, The UK.'),
-                    SizedBox(width: 70,),
-                    GestureDetector(
-                      onTap: () {
-                        launch('https://www.plymouth.ac.uk/');
-                      },
-                      child: Image.asset('lib/images/1.png', width: 60,),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            gradient: const LinearGradient(colors: <Color>[
+                              Color(0xFF77CB7E),
+                              Color(0xFFB4CAE8),
+                            ]),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                ('Logout'),
+                                style: const TextStyle(
+                                    color: const Color.fromARGB(255, 70, 66, 68),
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
+                    SizedBox(height: 50,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Row(
+                        children: [
+                          SizedBox(height: 50,),
+                          Text('Developed by undergraduate students\nfrom University of Plymouth, The UK.'),
+                          SizedBox(width: 70,),
+                          GestureDetector(
+                            onTap: () {
+                              launch('https://www.plymouth.ac.uk/');
+                            },
+                            child: Image.asset('lib/images/1.png', width: 60,),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20,),
                   ],
-                ),
-              ),
-              SizedBox(height: 20,),
-              ],
-            ),
-          ),
-        ),
-      );
+                )    ), ) 
+      
+    );
+  }
 }
